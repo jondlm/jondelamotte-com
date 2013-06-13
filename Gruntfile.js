c75ff50381facba5a3f114fcd667b574e67460ef
Jon de la Motte <jond@csgpro.com>
2013-06-13 11:27:53 -0700

    Made everything package into a single JS file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,21 @@
+// Just run `grunt` in the command line once you have run `npm install`
+// You can also run `grunt watch` to watch specific files
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
     uglify: {
       options: {
-        sourceMap: 'js/source-map.js'
+        sourceMap: 'js/source-map.js',
+        banner: '// By: Jon de la Motte\n' + 
+                '// On: ' + new Date() + '\n' +
+                '// \n' +
+                '// 3rd party libraries:\n' +
+                '//   Moment.js - v2.0.0 - momentjs.com\n\n'
       },
       main: {
         files: {
-          'js/main.min.js': ['js/main.js']
+          'js/package.min.js': ['js/moment.js', 'js/main.js' ]
         }
       }
     },
